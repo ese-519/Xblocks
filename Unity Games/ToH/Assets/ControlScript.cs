83902a8e68faec0a9681d7e096888c6b772ffcca
Sarvesh Patkar <psarvesh@users.noreply.github.com>
2016-12-20 20:50:22 -0500

    Add files via upload

---
+++ Unity Games/ToH/Assets/ControlScript.cs
@@ -0,0 +1,193 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO.Ports;
+
+public class ControlScript : MonoBehaviour
+{
+    //toh game
+    ObjectScript[] pawnlist;
+    SerialPort stream = new SerialPort("\\\\.\\COM9", 9600);
+    Vector3 c1low = new Vector3(12, 2, 0);
+    Vector3 c1mid = new Vector3(12, 4, 0);
+    Vector3 c1high = new Vector3(12, 6, 0);
+
+    Vector3 c2low = new Vector3(0, 2, 0);
+    Vector3 c2mid = new Vector3(0, 4, 0);
+    Vector3 c2high = new Vector3(0, 6, 0);
+
+    Vector3 c3low = new Vector3(-10, 2, 0);
+    Vector3 c3mid = new Vector3(-10, 4, 0);
+    Vector3 c3high = new Vector3(-10, 6, 0);
+
+
+    // Use this for initialization
+    void Start()
+    {
+        pawnlist = new ObjectScript[3];
+        stream.Open();
+        stream.ReadTimeout = 1;
+        for (int i = 0; i < pawnlist.Length; i++)
+        {
+            GameObject go = GameObject.Find("Object" + (i + 1));
+            if (go != null)
+            {
+                pawnlist[i] = go.GetComponent<ObjectScript>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stream.IsOpen)
+        {
+            try
+            {
+                string stringis = stream.ReadLine();
+               print(stringis);
+                string[] values = stringis.Split(' ');
+                int cylindernumber = int.Parse(values[3]);
+              /*  print("iin");
+                print("First is " + pawnlist[0].getPosition(1));
+                print("Second is " + pawnlist[1].getPosition(2));
+                print("Thrid is " + pawnlist[2].getPosition(3));*/
+
+                 if (values[2].Equals("A1"))
+                      {
+                          if (cylindernumber == 1)
+                          {
+                             if (!(pawnlist[1].getPosition(2) == c1low) && !(pawnlist[2].getPosition(3) == c1low))
+
+                                     pawnlist[0].MoveObject(c1low);
+
+                             else if (!(pawnlist[1].getPosition(2) == c1mid) && !(pawnlist[2].getPosition(3) == c1mid))
+                                  pawnlist[0].MoveObject(c1mid);
+
+                             else if (!(pawnlist[1].getPosition(2) == c1high) && !(pawnlist[2].getPosition(3) == c1high))
+                                  pawnlist[0].MoveObject(c1high);
+
+                          }
+                        else  if (cylindernumber == 2)
+                          {
+
+                              if (!(pawnlist[1].getPosition(2) == c2low) && !(pawnlist[2].getPosition(3) == c2low))
+                                  pawnlist[0].MoveObject(c2low);
+
+                              else if (!(pawnlist[1].getPosition(2) == c2mid) && !(pawnlist[2].getPosition(3) == c2mid))
+                                  pawnlist[0].MoveObject(c2mid);
+
+                              else if (!(pawnlist[1].getPosition(2) == c2high) && !(pawnlist[2].getPosition(3) == c2high))
+                                  pawnlist[0].MoveObject(c2high);
+
+                          }
+                         else if (cylindernumber == 3)
+                          {
+                            //  print("Inside here - A1 3");
+                              if (!(pawnlist[1].getPosition(2) == c3low) && !(pawnlist[2].getPosition(3) == c3low))
+                              {
+                               //   print("Here 1");
+                                  pawnlist[0].MoveObject(c3low);
+                              }
+
+
+                              else if (!(pawnlist[1].getPosition(2) == c3mid) && !(pawnlist[2].getPosition(3) == c3mid))
+                              {
+                                //  print("HEre 2");
+                                  pawnlist[0].MoveObject(c3mid);
+                              }
+
+                              else if (!(pawnlist[1].getPosition(2) == c3high) && !(pawnlist[2].getPosition(3) == c3high))
+                              {
+                                 // print("Here 3");
+                                  pawnlist[0].MoveObject(c3high);
+                              }
+                          }
+                      }
+                      //a2 
+                      if (values[2].Equals("A2"))
+                      {
+                          if (cylindernumber == 1)
+                          {
+                              if (!(pawnlist[0].getPosition(1) == c1low) && !(pawnlist[2].getPosition(3) == c1low))
+                                  pawnlist[1].MoveObject(c1low);
+
+                              else if (!(pawnlist[0].getPosition(1) == c1mid) && !(pawnlist[2].getPosition(3) == c1mid))
+                                  pawnlist[1].MoveObject(c1mid);
+
+                              else if (!(pawnlist[0].getPosition(1) == c1high) && !(pawnlist[2].getPosition(3) == c1high))
+                                  pawnlist[1].MoveObject(c1high);
+
+                          }
+                          else if (cylindernumber == 2)
+                          {
+                              if (!(pawnlist[0].getPosition(1) == c2low) && !(pawnlist[2].getPosition(3) == c2low))
+                                  pawnlist[1].MoveObject(c2low);
+
+                              else if (!(pawnlist[0].getPosition(1) == c2mid) && !(pawnlist[2].getPosition(3) == c2mid))
+                                  pawnlist[1].MoveObject(c2mid);
+
+                              else if (!(pawnlist[0].getPosition(1) == c2high) && !(pawnlist[2].getPosition(3) == c2high))
+                                  pawnlist[1].MoveObject(c2high);
+
+                          }
+                          else if (cylindernumber == 3)
+                          {
+                              if (!(pawnlist[0].getPosition(1) == c3low) && !(pawnlist[2].getPosition(3) == c3low))
+                                  pawnlist[1].MoveObject(c3low);
+
+                              else if (!(pawnlist[0].getPosition(1) == c3mid) && !(pawnlist[2].getPosition(3) == c3mid))
+                                  pawnlist[1].MoveObject(c3mid);
+
+                              else if (!(pawnlist[0].getPosition(1) == c3high) && !(pawnlist[2].getPosition(3) == c3high))
+                                  pawnlist[1].MoveObject(c3high);
+
+                          }
+                      }
+                      //a3
+                      if (values[2].Equals("A3"))
+                      {
+                          if (cylindernumber == 1)
+                          {
+                              if (!(pawnlist[1].getPosition(2) == c1low) && !(pawnlist[0].getPosition(1) == c1low))
+                                  pawnlist[2].MoveObject(c1low);
+
+                              else if (!(pawnlist[1].getPosition(2) == c1mid) && !(pawnlist[0].getPosition(1) == c1mid))
+                                  pawnlist[2].MoveObject(c1mid);
+
+                              else if (!(pawnlist[1].getPosition(2) == c1high) && !(pawnlist[0].getPosition(1) == c1high))
+                                  pawnlist[2].MoveObject(c1high);
+
+                          }
+                          else if (cylindernumber == 2)
+                          {
+                              if (!(pawnlist[1].getPosition(2) == c2low) && !(pawnlist[0].getPosition(1) == c2low))
+                                  pawnlist[2].MoveObject(c2low);
+
+                              else if (!(pawnlist[1].getPosition(2) == c2mid) && !(pawnlist[0].getPosition(1) == c2mid))
+                                  pawnlist[2].MoveObject(c2mid);
+
+                              else if (!(pawnlist[1].getPosition(2) == c2high) && !(pawnlist[0].getPosition(1) == c2high))
+                                  pawnlist[2].MoveObject(c2high);
+
+                          }
+                          else if (cylindernumber == 3)
+                          {
+                              if (!(pawnlist[1].getPosition(2) == c3low) && !(pawnlist[0].getPosition(1) == c3low))
+                                  pawnlist[2].MoveObject(c3low);
+
+                              else if (!(pawnlist[1].getPosition(2) == c3mid) && !(pawnlist[0].getPosition(1) == c3mid))
+                                  pawnlist[2].MoveObject(c3mid);
+
+                              else if (!(pawnlist[1].getPosition(2) == c3high) && !(pawnlist[0].getPosition(1) == c3high))
+                                  pawnlist[2].MoveObject(c3high);
+
+                          }
+                      }
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Unity Games/ToH/Assets/ObjectScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectScript : MonoBehaviour
+{
+    // public Transform target;
+    public float speed = 10f;
+    public Vector3 TargetPosition;
+    public bool move = false;
+    float posx;
+    float posy;
+    GameObject Pawn1;
+
+    // Use this for initialization
+    void Start()
+    {
+        // TargetPosition = new Vector3(-2, 1.5f, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, TargetPosition, step);
+        }
+    }
+
+    public void MoveObject(Vector3 recieved)
+    {
+        move = true;
+        TargetPosition = recieved;
+    }
+
+    public Vector3 getPosition(int i)
+    {
+        return GameObject.Find("Object" + i).transform.position;
+    }
+}
